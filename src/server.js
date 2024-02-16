2ac1f21f9917db934f5373be14b778979992504e
Mauricios73 <mauricioportela73@gmail.com>
2024-02-15 22:38:14 -0300

    test cors

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ mongoose.connect(MONGODB_URI, {
 app.use(cors());
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
@@ -28,7 +28,7 @@ app.use(cors());
     }
 });
   
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
