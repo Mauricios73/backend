d45e568ed387a93b112fbe96b2568ba14833970f
Mauricios73 <mauricioportela73@gmail.com>
2024-02-15 21:37:34 -0300

    inicial commit

---
+++ src/routes.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = express.Router();
+
+// app.use(bodyParser.json());
+
+//________________LOGIN__________________
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+routes.post('/users', async (req, res) => {
+  const { email, password } = req.body;
+  
+  try {
+    // const user = users.find(user => user.email === email && user.password === password);
+    const user = await User.findOne({ email, password });
+
+    if (user) {
+      return res.status(200).json(user);
+    }
+    return res.status(401).json({ message: 'Credenciais inválidas' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+//______________AGENDAMENTOS______________
+
+const FormModel = mongoose.model('agendamentos', {
+  name: String,
+  phone: String,
+  email: String,
+  date: Date
+});
+
+routes.post('/agendamentos', (req, res) => {
+  const { name, phone, email, date } = req.body;
+
+  const formData = new FormModel({
+    name,
+    phone,
+    email,
+    date
+  });
+
+  formData.save()
+    .then(() => {
+      console.log('Os dados foram salvos.');
+      res.status(200).json({ message: 'Os dados foram salvos.' });
+    })
+    .catch(err => {
+      console.log('Erro ao salvar os dados:', err);
+      res.status(500).json({ error: 'Erro ao salvar os dados.' });
+    });
+});
+
+// routes.get('/', (req, res) => {
+//   res.send('Hello World4');
+// });
+
+module.exports = routes; 
---
+++ src/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors({
+    origin: '*',
+    credentials: true 
+}));
+
+  app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+  
+app.use(cors());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Erro de conexão com o MongoDB:'));
+db.once('connected', () => {
+    console.log('Conexão com o MongoDB estabelecida com sucesso!');
+    app.listen(PORT, () => {
+        console.log(`Servidor ouvindo na port ${PORT}`);
+    });
+});
+
