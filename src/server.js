8aca393afdccb6d11ac2f5189c526d7c53a32056
Mauricios73 <mauricioportela73@gmail.com>
2024-02-15 22:34:10 -0300

    teste de cors

--- src/server.js
+++ src/server.js
@@ -14,13 +14,10 @@ mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true
 });
 
-app.use(cors({
-    origin: '*',
-    credentials: true 
-}));
+app.use(cors());
 
   app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
@@ -31,7 +28,7 @@ app.use(cors({
     }
 });
   
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
