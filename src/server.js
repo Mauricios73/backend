a72121b5e1ad385b9a01e06d2b72b26603bfee4c
Mauricios73 <mauricioportela73@gmail.com>
2024-02-15 23:01:23 -0300

    teste de cors

--- src/server.js
+++ src/server.js
@@ -14,10 +14,13 @@ mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true
 });
 
-app.use(cors());
+// app.use(cors({
+//     origin: 'https://nf-style.vercel.app',
+//     credentials: true 
+// }));
 
   app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://nf-style.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
