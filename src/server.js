b80410d3629c9a033474b3f38e7552dcd8a74cb0
Mauricios73 <mauricioportela73@gmail.com>
2024-02-28 15:42:16 -0300

    vercel.json modificado

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use(cors());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use('/', routes);
 
 const db = mongoose.connection;
 
